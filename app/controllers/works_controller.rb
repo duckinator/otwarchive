87daf571aacccb69d722609a086f553f82535e34
Marie Markwell <me@marie.so>
2014-08-21 04:07:18 -0400

    Deduplicate some code in WorksController#marktoread.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -716,11 +716,11 @@ public
     @work = Work.find(params[:id])
     Reading.mark_to_read_later(@work, current_user)
     read_later_path = user_readings_path(current_user, :show => 'to-read')
-    if @work.marked_for_later?(current_user)
-      flash[:notice] = ts("This work was <strong>removed</strong> from your #{view_context.link_to('Marked for Later list', read_later_path)}. It may take a while for changes to show up.").html_safe
-    else
-      flash[:notice] = ts("This work was <strong>added</strong> to your #{view_context.link_to('Marked for Later list', read_later_path)}. It may take a while for changes to show up.").html_safe
-    end
+
+    added_removed_text = @work.marked_for_later?(current_user) ? 'removed' : 'added'
+
+    flash[:notice] = ts("This work was <strong>#{added_removed_text}</strong> from your #{view_context.link_to('Marked for Later list', read_later_path)}. It may take a while for changes to show up.").html_safe
+
     redirect_to(request.env["HTTP_REFERER"] || root_path)
   end
 
